d87ebab90bf0a618c9e76003766f107f37606328
Sergei <sepolish@student.jyu.fi>
2019-09-14 23:02:43 +0300

    2.8: puhelinluettelo step3

--- puhelinluettelo/src/components/App.js
+++ puhelinluettelo/src/components/App.js
@@ -2,13 +2,17 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
+    { 
+      name: 'Arto Hellas',
+      number: '040-1234567',
+    },
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    if (newName.length === 0) {
+    if (newName.length === 0 || newNumber.length === 0) {
       return
     }
     const unique = persons.find(p => p.name === newName) === undefined
@@ -17,8 +21,13 @@ const App = () => {
       return
     }
 
-    setPersons([...persons, { name: newName }])
+    const newPerson = { 
+      name: newName,
+      number: newNumber,
+    }
+    setPersons([...persons, newPerson])
     setNewName("")
+    setNewNumber("")
   }
 
   const handleNewNameInput = event => {
@@ -26,8 +35,13 @@ const App = () => {
     setNewName(name)
   }
 
+  const handleNewNumberInput = event => {
+    const number = event.target.value
+    setNewNumber(number)
+  }
+
   const mapPersons = persons.map((p, i) => 
-    <li key={p.name+i} >{p.name}</li>
+    <li key={p.name+i} >{p.name} {p.number}</li>
   )
 
 
@@ -40,6 +54,13 @@ const App = () => {
           <input 
             value={newName}  
             onChange={handleNewNameInput}
+          />
+        </div>
+        <div>
+          number: 
+          <input 
+            value={newNumber}  
+            onChange={handleNewNumberInput}
           />
         </div>
         <div>
