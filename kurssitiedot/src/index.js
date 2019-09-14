31878ff36666ab880255fa8ec15c8bc8be3d8343
Sergei <sepolish@student.jyu.fi>
2019-09-14 19:34:57 +0300

    2.4: kurssitiedot step9

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 
 const Header = (props) => {
-  return <h1>{props.name}</h1>
+  return <h2>{props.name}</h2>
 }
 
 const Part = props => (
@@ -45,32 +45,57 @@ const Course = (props) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'Redux',
-        exercises: 11
-      },
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
+  const mapCourses = () => courses.map((x, i) => 
+      <Course key={x.name + i} course={x} />
+  )
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {mapCourses()}
     </div>
   )
 }
